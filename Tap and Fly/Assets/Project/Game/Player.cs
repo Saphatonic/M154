f808a474e1612e32704de17b4bdceaa65851e450
Saphatonic <Saphatonic@gmail.com>
2015-11-16 11:35:18 +0100

    Updated Game

---
+++ Tap and Fly/Assets/Project/Game/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private Vector3 position;
+	
+	public int speed = 5;
+	public int rotationSpeed = 1;
+	public float gravity = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		position = transform.position;
+
+		// Rotate
+		transform.Rotate(Vector3.forward * rotationSpeed);
+
+		// Update Gravity 
+		gravity += 0.2f;
+		// Update Position
+		// position.y -= gravity * Time.deltaTime;
+		transform.position = position;
+	}
+}
