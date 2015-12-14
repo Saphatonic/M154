3a45e1675d9d6daadd36f1aa12ba3683ddfa5e8a
Saphatonic <Saphatonic@gmail.com>
2015-12-14 09:47:54 +0100

    Added object

---
+++ Tap and Fly/Assets/Project/Game/Obstacles/Pendulum.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pendulum : MonoBehaviour {
+
+	public HingeJoint2D Hinge;
+
+	// Update is called once per frame
+	void Update () {
+		if(Hinge.jointAngle >= Hinge.limits.max)
+		{
+			ChangeDirection();
+		} 
+		else if(Hinge.jointAngle <= Hinge.limits.min)
+		{
+			ChangeDirection();
+		}
+	}
+
+	private void ChangeDirection()
+	{
+		var motor = Hinge.motor;
+		motor.motorSpeed *= -1;
+		Hinge.motor = motor;
+	}
+}
