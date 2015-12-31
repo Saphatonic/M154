99bfb9a10fa5d426af483a532cf90ba7670166bb
Saphatonic <Saphatonic@gmail.com>
2015-12-31 12:14:54 +0100

    Changed tutorial screen

--- Tap and Fly/Assets/Project/Game/Input/SingleTouch.cs
+++ Tap and Fly/Assets/Project/Game/Input/SingleTouch.cs
@@ -1,63 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections;
 
-public class SingleTouch {
+public class SingleTouch
+{
 
     public bool Began;
     public bool Moved;
     public bool Stationary;
     public bool Ended;
 
-    public bool DisableMovement = false;
-
-	// Update is called once per frame
-	public void Update () {
-        if (DisableMovement)
-        {
-            return;
-        }
-
+    public void Update()
+    {
         // Reset
         Began = false;
         Moved = false;
         Stationary = false;
         Ended = false;
 
-        // Get Input
-	    if (Input.touchCount > 0) {
-            var phase = Input.GetTouch(0).phase;
-            switch (phase)
-            {
-                case TouchPhase.Began:
-                    Began = true;
-                    break;
-                case TouchPhase.Moved:
-                    Moved = true;
-                    break;
-                case TouchPhase.Stationary:
-                    Stationary = true;
-                    break;
-                case TouchPhase.Ended:
-                    Ended = true;
-                    break;
-            }
-	    }
-        // Debug Mouse
+        if (Input.touchCount > 0)
+        {
+            Update(Input.GetTouch(0));
+        }
+
         if (Debug.isDebugBuild)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Began = true;
-            }
-            else if (Input.GetMouseButton(0))
-            {
-                Stationary = true;
-                Moved = true;
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                Ended = true;
-            }    
+            DebugUpdate();
         }
-	}
+    }
+
+    // Update is called once per frame
+    public void Update(Touch touch)
+    {
+        // Is Touch over UI
+        int pointerID = touch.fingerId;
+        if (EventSystem.current.IsPointerOverGameObject(pointerID))
+        {
+            return;
+        }
+
+        var phase = touch.phase;
+        switch (phase)
+        {
+            case TouchPhase.Began:
+                Began = true;
+                break;
+            case TouchPhase.Moved:
+                Moved = true;
+                break;
+            case TouchPhase.Stationary:
+                Stationary = true;
+                break;
+            case TouchPhase.Ended:
+                Ended = true;
+                break;
+        }
+    }
+
+    public void DebugUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Began = true;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Stationary = true;
+            Moved = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Ended = true;
+        }
+    }
 }
